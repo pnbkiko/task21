e9cf4da83767ac9b26269b2eca0e1ac40288be67
user45 <sundu.dunduk@mail.ru>
2026-01-13 10:53:45 +0300

    1

---
+++ src/task2.java
@@ -0,0 +1,15 @@
+
+interface Summator {
+    int sum(int n1, int n2);
+}
+
+public class task2 {
+    public static void main(String[] args) {
+        // сохраните лямбду в переменную
+        // лямбда должна складывать два целых числа
+        Summator summator = (n1, n2) -> n1+n2;
+
+        // допишите код, использующий лямбду
+        System.out.println("Сумма 23 и 65 равна " + summator.sum(23,65));
+    }
+}
